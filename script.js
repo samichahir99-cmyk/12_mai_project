7691121061cb4ac288c925959470016553ba6eb5
samichahir99-cmyk <samichahir99@gmail.com>
2026-05-16 14:26:55 +0100

    java log

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const nom=document.getElementById('name');
 const email=document.getElementById('email');
 const password=document.getElementById('password');
 
-const button=document.getElementById('btn');
+const button1=document.getElementById('btn');
+const button2=document.getElementById('btn2');
 
-button.addEventListener('click', function() {
+button1.addEventListener('click', function() {
     const name=nom.value;
     const mail=email.value;
     const pass=password.value;
@@ -20,6 +21,30 @@ button.addEventListener('click', function() {
         localStorage.setItem('users', JSON.stringify(users))
 
         alert('Inscription réussie !' + "nom : " + nom.value + " email : " + email.value + " password : " + password.value);
+        nom.value=email.value=password.value='';
+    }
+});
+
+button2.addEventListener('click', function() {
+    const name=nom.value;
+    const mail=email.value;
+    const pass=password.value;
+
+    if (nom.value==='' || email.value==='' || password.value==='') 
+    {
+        alert('Veuillez remplir tous les champs');
+    } 
+    else
+    {
+        const users=JSON.parse(localStorage.getItem('users') || '[]');
+        if (users.nom===name && users.email===mail && users.password===pass)
+        {
+            alert('Login reussie');
+        }
+        else
+        {
+            alert('Login reussie');
+        }
         nom.value=email.value=password.value='';
     }
 });
