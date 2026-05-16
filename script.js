a067a8b4da06d62e9dd170ee36d884a74b2302e4
samichahir99-cmyk <samichahir99@gmail.com>
2026-05-16 13:56:15 +0100

    java1

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const nom=document.getElementById('name')
-const email=document.getElementById('email')
-const password=document.getElementById('password')
-const button=document.getElementById('btn')
+const nom=document.getElementById('name');
+const email=document.getElementById('email');
+const password=document.getElementById('password');
 
-button.addEventListener('click', function(){
+const button=document.getElementById('btn');
 
-    const name=nom.value.trim()
-    const mail=email.value.trim()
-    const pass=password.value.trim()
+button.addEventListener('click', function() {
+    const name=nom.value;
+    const mail=email.value;
+    const pass=password.value;
 
-    if (nom.value === '' || email.value === '' || password.value === '')
+    if (nom.value==='' || email.value==='' || password.value==='') 
     {
-        return alert('Veuillez remplir tous les champs');
-    }
-    
-    const users = JSON.parsel(localStorage.getItem('users' ) || '[]');
-    users.push({ nom:name , email: mail , password: pass });
-    localStorage.setItem('users',JSON.stringify(users));
-    
-    alert('inscription reussie !' + "nom:" + nom.value + "email:" + email.value + "password:" + password.value )
-    nom.value = email.value = password.value ='';
-});
+        alert('Veuillez remplir tous les champs');
+    } 
+    else
+    {
+        const users=JSON.parse(localStorage.getItem('users') || '[]');
+        users.push({nom: name,email: mail,password: pass})
+        localStorage.setItem('users', JSON.stringify(users))
 
+        alert('Inscription réussie !' + "nom : " + nom.value + " email : " + email.value + " password : " + password.value);
+        nom.value=email.value=password.value='';
+    }
+});
