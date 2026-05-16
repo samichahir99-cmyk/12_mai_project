3cece548fcea2607473a66d33a745a33c8b48aba
samichahir99-cmyk <samichahir99@gmail.com>
2026-05-16 13:46:52 +0100

    java

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const nom=document.getElementById('name')
 const email=document.getElementById('email')
 const password=document.getElementById('password')
-
 const button=document.getElementById('btn')
 
 button.addEventListener('click', function(){
+
+    const name=nom.value.trim()
+    const mail=email.value.trim()
+    const pass=password.value.trim()
+
     if (nom.value === '' || email.value === '' || password.value === '')
     {
-        alert('Veuillez remplir tous les champs');
+        return alert('Veuillez remplir tous les champs');
     }
-    else{
-        alert('inscription reussie !' + "nom:" + nom.value + "email:" + email.value + "password:" + password.value )
-    }
+    
+    const users = JSON.parsel(localStorage.getItem('users' ) || '[]');
+    users.push({ nom:name , email: mail , password: pass });
+    localStorage.setItem('users',JSON.stringify(users));
+    
+    alert('inscription reussie !' + "nom:" + nom.value + "email:" + email.value + "password:" + password.value )
+    nom.value = email.value = password.value ='';
 });
 
